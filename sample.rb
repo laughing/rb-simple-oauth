4c9234b5679ac4a8d671ed333af363924273052a
laughing <laughing07+github@gmail.com>
2009-12-17 01:28:15 -0500

    サンプル書いた 	modified:   sample.rb

--- sample.rb
+++ sample.rb
@@ -4,6 +4,7 @@
 require 'simple-oauth'
 require 'rubygems'
 require 'json'
+require 'readline'
 
 # ここを置き換える
 CONSUMER_KEY = 'CONSUMER-KEY'
@@ -13,12 +14,14 @@ TOKEN_SECRET = 'ACCESS-TOKEN-SECRET'
 
 simple_oauth = SimpleOAuth.new(CONSUMER_KEY, CONSUMER_SECRET, TOKEN, TOKEN_SECRET)
 
+# 認証してから 
 # Tweetの投稿
+response = simple_oauth.request_token('http://twitter.com/oauth/request_token')
+puts "access PIN => " + response[:authorize]
+response = simple_oauth.access_token('http://twitter.com/oauth/access_token', Readline.readline('PIN > ').strip)
 response = simple_oauth.post('http://twitter.com/statuses/update.json', {
-  :status => "こんにちは！この投稿はテストです。 : #{Time.now}"
-})
+                              :status => Readline.readline('update status > ').strip})
 raise "Request failed: #{response.code}" unless response.code.to_i == 200
-
 # TimeLineの取得
 response = simple_oauth.get('http://twitter.com/statuses/friends_timeline.json?count=5')
 raise "Request failed: #{response.code}" unless response.code.to_i == 200
